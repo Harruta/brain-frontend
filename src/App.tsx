26f00e4e748a08ee3f3c66f29476f5dea1e71431
Harruta <haruubusinesss@gmail.com>
2025-02-27 23:40:53 +0530

    completed card component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,12 @@ function App() {
     <div>
       <Button variant="primary" text="Add content" startIcon={<PlusIcon/>} />
       <Button variant="secondary" text="share" startIcon={<ShareIcon/>} />
-      <Card />
+      <div className="flex">
+        <Card type="twitter" link="https://x.com/codebyharu/status/1895156519546822742"
+         title="First tweet"/>
+        <Card type="youtube" link="https://www.youtube.com/watch?v=JGwWNGJdvx8"
+        title="First vid"/>
+      </div>
     </div>
   );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,25 +1,48 @@
 import { ShareIcon } from "../icons/ShareIcon";
 
-export function Card() {
-    return <div>
-       <div className="p-4 bg-white rounded-md border-gray-200 max-w-72 border">
-       <div className="flex justify-between">
-            <div className="flex items-center text-md">
-                <div className="text-gray-500 pr-2">
-                    <ShareIcon />
+interface CardProps {
+    title: string;
+    link: string;
+    type: "twitter" | "youtube"; 
+}
+
+export function Card({ title, link, type }: CardProps) {
+    return (
+        <div className="p-4 bg-white rounded-md border-gray-200 max-w-72 border">
+            <div className="flex justify-between">
+                <div className="flex items-center text-md">
+                    <div className="text-gray-500 pr-2">
+                        <ShareIcon />
+                    </div>
+                    {title}
                 </div>
-                Project Ideas
-            </div>
                 <div className="flex items-center">
                     <div className="pr-2 text-gray-500">
-                        <ShareIcon />
+                        <a href={link} target="blank">
+                            <ShareIcon/>
+                        </a>
                     </div>
                     <div className="text-gray-500">
                         <ShareIcon />
                     </div>
                 </div>
-            
+            </div>
+            <div className="pt-4">
+            {type === "youtube" && (
+                <iframe className="w-full" src={link.replace("watch","embed")
+                    .replace("?v=","/")
+                } title="YouTube video
+                player"frameBorder="0" allow="accelerometer; autoplay; 
+                clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            )}
+            </div>
+
+            <div className="pt-4">
+            {type === "twitter" &&
+            <blockquote className="twitter-tweet">
+                <a href={link.replace("x.com", "twitter.com")}></a>
+            </blockquote>}
+            </div>
         </div>
-       </div>
-    </div>
+    );
 }
