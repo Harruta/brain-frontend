315f4641a8ee446db3f0b98cbe1ac874baf08655
Harruta <haruubusinesss@gmail.com>
2025-02-28 18:10:46 +0530

    card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Button } from "./components/Button";
+import { useState } from "react";
+import { CreateContentModel } from "./components/CreateContentModel";
 import { Card } from "./components/card";
 import { PlusIcon } from "./icons/Pluesicon";
 import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
+  const [modelOpen, setModelOpen] = useState(true);
   return (
     <div className="p-4">
+      <CreateContentModel open={modelOpen} onClose={() => setModelOpen(false)} />
+
       <div className="flex justify-end gap-4">
        <Button variant="primary" text="Add content" startIcon={<PlusIcon/>} />
        <Button variant="secondary" text="share" startIcon={<ShareIcon/>} />
