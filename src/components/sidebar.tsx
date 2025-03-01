433ccd4f538eb0ec42c8febee005824a61413fd7
Harruta <haruubusinesss@gmail.com>
2025-03-01 21:13:49 +0530

    sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,12 +1,19 @@
 import { TwitterIcon } from "../icons/TwitteIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
 import { SidebarItem } from "./sidebaritem";
+import { Logo } from "../icons/Logo";
 
 export function Sidebar() {
-    return <div className="h-screen bg-white w-72 fixed left-0 top-0">
-        <div className="pt-4">
-            <SidebarItem text="Twitter" icon={<TwitterIcon/>} />
-            <SidebarItem text="Youtube" icon={<YoutubeIcon/>} />
+    return (
+        <div className="h-screen bg-white w-72 fixed left-0 top-0 pl-6 pt-6">
+            <div className="flex items-center text-2xl font-bold pl-3 space-x-2">
+                <Logo />
+                <span className="ml-[-4px]">Brainly</span>
+            </div>
+            <div className="pt-6">
+                <SidebarItem text="Twitter" icon={<TwitterIcon />} />
+                <SidebarItem text="Youtube" icon={<YoutubeIcon />} />
+            </div>
         </div>
-    </div>
+    );
 }
