a24e6b1faae3d4e37c5cbe07b0172dc8184cd275
Harruta <haruubusinesss@gmail.com>
2025-03-01 23:07:17 +0530

    input

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,7 +6,7 @@ import { Logo } from "../icons/Logo";
 export function Sidebar() {
     return (
         <div className="h-screen bg-white w-72 fixed left-0 top-0 pl-6 pt-6">
-            <div className="flex items-center text-2xl font-bold pl-3 space-x-2">
+            <div className="flex items-center text-2xl font-bold pl-2 space-x-2">
                 <Logo />
                 <span className="ml-[-4px]">Brainly</span>
             </div>
