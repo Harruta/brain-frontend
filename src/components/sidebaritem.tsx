945fc7d54205d743f1b3dfac165db3061508a7b1
Harruta <haruubusinesss@gmail.com>
2025-03-01 22:21:21 +0530

    imporovements

--- src/components/sidebaritem.tsx
+++ src/components/sidebaritem.tsx
@@ -5,9 +5,14 @@ export function SidebarItem({ text, icon}: {
     icon: ReactElement;
 }) {
     return (
-        <div className="flex items-center gap-4 p-4 text-gray-500">
-            {icon}
-            {text}
+        <div className="flex items-center gap-4 p-4 text-gray-500 cursor-pointer
+        hover:bg-gray-200 rounded max-w-48 pl-4 transition-all duration-150">
+            <div className="pr-2">
+              {icon}
+            </div>
+            <div>
+                {text}
+            </div>
         </div>
     );
 }
