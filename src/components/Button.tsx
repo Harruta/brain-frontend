e9805465c4b15f1098275a1cd0a9c558b61fa95c
Harruta <haruubusinesss@gmail.com>
2025-03-01 23:56:32 +0530

    added button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,8 +3,9 @@ import { ReactElement } from "react";
 interface ButtonProps {
     variant: "primary" | "secondary";
     text: string;
-    startIcon: ReactElement;
+    startIcon?: ReactElement;
     onClick?: () => void;
+    fullWidth?: boolean;
 }
 
 const variantClasses = {
@@ -14,10 +15,13 @@ const variantClasses = {
 
 const defaultStyles = "px-4 py-2 rounded-md font-light flex items-center space-x-2";
 
-export function Button({ variant, text, startIcon, onClick }: ButtonProps) {
+export function Button({ variant, text, startIcon, onClick, fullWidth }: ButtonProps) {
     return (
-        <button onClick={onClick} className={`${variantClasses[variant]} ${defaultStyles}`}>
-            {startIcon}
+        <button 
+            onClick={onClick} 
+            className={`${variantClasses[variant]} ${defaultStyles} ${fullWidth ? 'w-full flex justify-center items-center' : ""}`}
+        >
+            {startIcon && <span>{startIcon}</span>}
             <span>{text}</span>
         </button>
     );
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,14 +2,15 @@ import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 export function Signup() {
-    return <div className="h-screen w-screen bg-gray-200 flex
-    justify-center items-center">
-        <div className="bg-white rounded border min-w-48">
-            <Input placeholder="username"/>
-            <Input placeholder="password"/>
-
-            <Button variant="primary" text="Signup"/>
+    return (
+        <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
+            <div className="bg-white rounded-xl min-w-48 p-6">
+                <Input placeholder="username" className="border border-gray" />
+                <Input placeholder="password" className="border border-gray" />
+                <div className="flex justify-center mt-4">
+                    <Button variant="primary" text="Signup" fullWidth={true} />
+                </div>
+            </div>
         </div>
-
-    </div>
+    );
 }
