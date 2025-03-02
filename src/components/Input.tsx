777d4f895782154525f0dbd0c5d417b3af324956
Harruta <haruubusinesss@gmail.com>
2025-03-02 20:06:01 +0530

    axios: connected frontend with the backend

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,16 @@
-export function Input({ onChange, placeholder }: { onChange: (event: React.ChangeEvent<HTMLInputElement>) => void; placeholder?: string }) {
-    return (
-        <div>
-            <input 
-                placeholder={placeholder} 
-                type="text" 
-                className="px-4 py-2 border rounded m-2" 
-                onChange={onChange} 
-            />
-        </div>
-    );
-}
+import { forwardRef } from "react";
+
+export const Input = forwardRef<HTMLInputElement, { placeholder?: string }>(
+    ({ placeholder }, ref) => {
+        return (
+            <div>
+                <input
+                    placeholder={placeholder}
+                    type="text"
+                    className="px-4 py-2 border rounded m-2"
+                    ref={ref || undefined} 
+                />
+            </div>
+        );
+    }
+);
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,14 +1,29 @@
+import axios from "axios";
+import { useRef } from "react";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { BACKEND_URL } from "../Config";
 
 export function Signup() {
+    const usernameRef = useRef<HTMLInputElement | null>(null);
+    const passwordRef = useRef<HTMLInputElement | null>(null);
+
+    async function signup() {
+        const username = usernameRef.current?.value;
+        const password = passwordRef.current?.value;
+        axios.post(BACKEND_URL + "/api/v1/signup", {
+            username,
+            password
+        })
+    }
+
     return (
         <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
             <div className="bg-white rounded-xl min-w-48 p-6">
-                <Input placeholder="username" className="border border-gray" />
-                <Input placeholder="password" className="border border-gray" />
+                <Input ref={usernameRef} placeholder="Username" />
+                <Input ref={passwordRef} placeholder="Password" />
                 <div className="flex justify-center mt-4">
-                    <Button loading={false} variant="primary" text="Signup" fullWidth={true} />
+                    <Button loading={false} variant="primary" text="Signup" fullWidth />
                 </div>
             </div>
         </div>
