8cd288290ca6e7c5b7cfc91a9379c64133a7a1a5
Harruta <haruubusinesss@gmail.com>
2025-03-02 19:29:28 +0530

    router

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,18 @@
-
-import Dashboard from './Dashboard'
-import { Signup } from './Signup'
+import { Signin } from './Signin';
+import { Signup } from './Signup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
 
 const App = () => {
   return (
-    <div>
-      <Signup/>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
