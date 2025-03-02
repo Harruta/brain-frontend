d60ff00e1ea311c38092071ef7e3ef8dfa337c98
Harruta <haruubusinesss@gmail.com>
2025-03-02 14:34:40 +0530

    signin and signup

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,7 @@ interface ButtonProps {
     startIcon?: ReactElement;
     onClick?: () => void;
     fullWidth?: boolean;
+    loading?: boolean;
 }
 
 const variantClasses = {
@@ -15,11 +16,12 @@ const variantClasses = {
 
 const defaultStyles = "px-4 py-2 rounded-md font-light flex items-center space-x-2";
 
-export function Button({ variant, text, startIcon, onClick, fullWidth }: ButtonProps) {
+export function Button({ variant, text, startIcon, onClick, fullWidth, loading }: ButtonProps) {
     return (
         <button 
             onClick={onClick} 
-            className={`${variantClasses[variant]} ${defaultStyles} ${fullWidth ? 'w-full flex justify-center items-center' : ""}`}
+            className={`${variantClasses[variant]} ${defaultStyles} ${fullWidth ? 'w-full flex justify-center items-center' : ''} ${loading ? 'opacity-45 cursor-not-allowed' : ''}`}
+            disabled={loading}
         >
             {startIcon && <span>{startIcon}</span>}
             <span>{text}</span>
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,14 +1,14 @@
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
-export function Signup() {
+export function Signin() {
     return (
         <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
             <div className="bg-white rounded-xl min-w-48 p-6">
                 <Input placeholder="username" className="border border-gray" />
                 <Input placeholder="password" className="border border-gray" />
                 <div className="flex justify-center mt-4">
-                    <Button loading={false} variant="primary" text="Signup" fullWidth={true} />
+                    <Button loading={false} variant="primary" text="Signin" fullWidth={true} />
                 </div>
             </div>
         </div>
