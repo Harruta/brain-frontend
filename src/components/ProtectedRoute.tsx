7f74cf7e42ea58e36cedde2eebb565c89a3c0a53
Harruta <haruubusinesss@gmail.com>
2025-03-17 22:44:11 +0530

    added protected route

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  element: JSX.Element;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
+  const isAuthenticated = !!localStorage.getItem("token"); 
+
+  return isAuthenticated ? element : <Navigate to="/signin" />;
+};
+
+export default ProtectedRoute;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,9 @@
-import { Signin } from './Signin';
-import SharedContent from '../components/SharedContent';
-import { Signup } from './Signup';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './Dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signin } from "./Signin";
+import SharedContent from "../components/SharedContent";
+import { Signup } from "./Signup";
+import Dashboard from "./Dashboard";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Signin />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
         <Route path="/share/:sharelink" element={<SharedContent />} />
       </Routes>
     </BrowserRouter>
